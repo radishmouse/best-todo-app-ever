38aba2bd4d08d71d41703fc40d028f0896494128
Chris Aquino <chris@radishmouse.com>
2019-11-15 12:48:44 -0500

    converts to classes

--- todo.py
+++ todo.py
@@ -1,35 +1,43 @@
+class Todo:
+    def __init__(self, text):
+        self.title = text
+        self.completed = False
 
-def print_todos(todos):
-    if len(todos) == 0:
-        print("You have nothing to do.")
-    else:
-        print('======= Pending ======')
-        for i, todo in enumerate(todos):
-            if not todo['completed']:
-                print(f"{i}: {todo['title']}")
-        print('======================')
-        print('======= Completed ======')
-        for i, todo in enumerate(todos):
-            if todo['completed']:
-                print(f"{i}: {todo['title']}")
-        print('======================')
+    def complete(self, is_complete=True):
+        self.completed = is_complete
 
-def add_todo(todos, item_text):
-    new_todo = {
-        'title': item_text,
-        'completed': False
-    }
-    todos.append(new_todo)
+class TodoApp:
+    def __init__(self):
+        self.todos = []
 
-def set_completed(todos, index):
-    try:
-        todo = todos[index]
-        todo['completed'] = True
-    except IndexError:
-        print("That todo does not exist.")
+    def add(self, text):
+        todo = Todo(text)
+        self.todos.append(todo)
 
-def print_menu():
-    message = """
+    def complete(self, index):
+        try:
+            self.todos[index].complete()
+        except:
+            print(f'No todo found at index {index}')
+        
+    def show(self):
+        if len(self.todos) == 0:
+            print("You have nothing to do.")
+        else:
+            print('======= Pending ======')
+            for i, todo in enumerate(self.todos):
+                if not todo.completed:
+                    print(f"{i}: {todo.title}")
+            print('======================')
+            print('======= Completed ======')
+            for i, todo in enumerate(self.todos):
+                if todo.completed:
+                    print(f"{i}: {todo.title}")
+            print('======================')
+
+
+    def print_menu(self):
+        message = """
     Todo App
 ==================
 0. quit
@@ -37,38 +45,35 @@ def print_menu():
 2. add a todo
 3. complete a todo
 """
-    print(message)
+        print(message)
 
-def get_choice(prompt="Choose one: "):
-    is_valid_choice = False
-    choice = 0
-    while not is_valid_choice:
-        try:    
-            choice = int(input(prompt))
-            is_valid_choice = True
-        except ValueError:
-            print("Please enter a number.")
-    return choice
+    def get_choice(self, prompt="Choose one: "):
+        is_valid_choice = False
+        choice = 0
+        while not is_valid_choice:
+            try:    
+                choice = int(input(prompt))
+                is_valid_choice = True
+            except ValueError:
+                print("Please enter a number.")
+        return choice
     
-def main():
-    todo_list = []
+    def main(self):
+        is_running = True
+        while is_running:
+            self.print_menu()
+            choice = self.get_choice()
+            if choice == 0:
+                print("K. Byeeee!")
+                is_running = False
+            elif choice == 1:
+                self.show()
+            elif choice == 2:
+                new_todo = input("Enter a todo: ")
+                self.add(new_todo)
+            elif choice == 3:            
+                index_to_delete = self.get_choice("Enter the index to complete: ")
+                self.complete(index_to_delete)
 
-    
-    is_running = True
-    while is_running:
-        print_menu()
-        choice = get_choice()
-        if choice == 0:
-            print("K. Byeeee!")
-            is_running = False
-        elif choice == 1:
-            print_todos(todo_list)
-        elif choice == 2:
-            new_todo = input("Enter a todo: ")
-            add_todo(todo_list, new_todo)
-        elif choice == 3:            
-            index_to_delete = get_choice("Enter the index to complete: ")
-            set_completed(todo_list, index_to_delete)
-        
-
-main()
+app = TodoApp()
+app.main()
