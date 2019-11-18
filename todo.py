23ed3c484cbf81d87de62245f1ee3c3346e22125
Chris Aquino <chrisaquino@radishmouse.local>
2019-11-18 14:03:52 -0500

    adds files from classroom demo

--- todo.py
+++ todo.py
@@ -1,74 +1,48 @@
+class Todo:
+    # instructions for how to construct
+    # a new Todo object
+    # Use the __init__() to create instances
+    # of the Todo class
+    # This method is also known as the "constructor"
+    # "dunder init" - "double underscore init"
+    def __init__(self, title, completed=False):
+        # Assume that a new todo
+        # is not already completed.
+        # So we set the default argument
+        # value to False
+        self.title = title
+        self.completed = completed
 
-def print_todos(todos):
-    if len(todos) == 0:
-        print("You have nothing to do.")
-    else:
-        print('======= Pending ======')
-        for i, todo in enumerate(todos):
-            if not todo['completed']:
-                print(f"{i}: {todo['title']}")
-        print('======================')
-        print('======= Completed ======')
-        for i, todo in enumerate(todos):
-            if todo['completed']:
-                print(f"{i}: {todo['title']}")
-        print('======================')
+    # behaviors
 
-def add_todo(todos, item_text):
-    new_todo = {
-        'title': item_text,
-        'completed': False
-    }
-    todos.append(new_todo)
 
-def set_completed(todos, index):
-    try:
-        todo = todos[index]
-        todo['completed'] = True
-    except IndexError:
-        print("That todo does not exist.")
-
-def print_menu():
-    message = """
-    Todo App
-==================
-0. quit
-1. print todos
-2. add a todo
-3. complete a todo
-"""
-    print(message)
-
-def get_choice(prompt="Choose one: "):
-    is_valid_choice = False
-    choice = 0
-    while not is_valid_choice:
-        try:    
-            choice = int(input(prompt))
-            is_valid_choice = True
-        except ValueError:
-            print("Please enter a number.")
-    return choice
+    # Examples of "Encapsulation"
+    # This means you hide the details of how
+    # your code works.
+    # You manage that yourself.
+    # You present an interface to anybody using
+    # your code, but you don't require
+    # them to know those details.
+    def update_title(self, new_title):
+        if self.title != new_title:
+            self.updated_on = '2019-11-18' # whatever the current date really is
+        self.title = new_title
+        
     
-def main():
-    todo_list = []
-
-    
-    is_running = True
-    while is_running:
-        print_menu()
-        choice = get_choice()
-        if choice == 0:
-            print("K. Byeeee!")
-            is_running = False
-        elif choice == 1:
-            print_todos(todo_list)
-        elif choice == 2:
-            new_todo = input("Enter a todo: ")
-            add_todo(todo_list, new_todo)
-        elif choice == 3:            
-            index_to_delete = get_choice("Enter the index to complete: ")
-            set_completed(todo_list, index_to_delete)
+    def update_completed(self, new_completed):
+        self.completed = new_completed
+        if self.completed:
+            self.completed_on = '2019-11-18' # no really, calculate the current date
         
 
-main()
+    def display(self):
+        # You must use the keyword
+        # "self" as the first argument
+        # so that an object can
+        # call the function.
+        # It links the function
+        # to the object.
+        print(self.title)
+
+    def __str__(self):
+        return f"{self.title} ({self.completed})"
---
+++ todolist.py
@@ -0,0 +1,30 @@
+from todo import Todo
+
+class TodoList:
+    def __init__(self):
+        # properties
+        # - todos
+        self.todos = []
+
+    def display(self):
+        for todo in self.todos:
+            print(todo)
+
+    def add(self, title):
+        # Instantiate a new Todo object
+        new_todo = Todo(title)
+        self.todos.append(new_todo)
+
+    def add_already_completed(self, title):
+        new_todo = Todo(title, True)
+        self.todos.append(new_todo)
+
+    def complete(self, index):
+        todo = self.todos[index]
+        # todo.completed = True
+        todo.update_completed(True)
+
+# behaviors
+# - print
+# - add
+# - complete
